398e22790ca06c41c8b10995ab0c25035dae11c0
jonfrost43 <jonfrost43@gmail.com>
2016-09-10 17:26:18 +0100

    implement MUI theme with Raleway font and AppBar component

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import {darkBlack} from 'material-ui/styles/colors';
+import AppBar from 'material-ui/AppBar';
 import { Router, Route, Link, browserHistory } from 'react-router';
 import Home from './component/Home';
 import AddRecipe from './component/AddRecipe';
@@ -9,13 +12,23 @@ import Recipe from './component/Recipe';
 
 injectTapEventPlugin();
 
+const muiTheme = getMuiTheme({
+	palette: {
+		textColor: darkBlack
+	},
+	fontFamily: 'Raleway, sans-serif'
+});
+
 ReactDOM.render(
-	<MuiThemeProvider>
-		<Router history={browserHistory}>
-			<Route path="/" activeClassName="active" component={Home} />
-			<Route path="/create" activeClassName="active" component={AddRecipe} />
-			<Route path="/recipe/:recipeId/:recipeName" activeClassName="active" component={Recipe} />
-		</Router>
+	<MuiThemeProvider muiTheme={muiTheme}>
+		<div>
+			<AppBar title="Cookbook" />
+			<Router history={browserHistory}>
+				<Route path="/" activeClassName="active" component={Home} />
+				<Route path="/create" activeClassName="active" component={AddRecipe} />
+				<Route path="/recipe/:recipeId/:recipeName" activeClassName="active" component={Recipe} />
+			</Router>
+		</div>
 	</MuiThemeProvider>,
 	document.getElementById('app')
 );
