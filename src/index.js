817f0bc718abd4cbd4da0326857efef39cc668ee
jonfrost43 <jonfrost43@gmail.com>
2016-09-10 14:43:59 +0100

    start the app with tap event plugin call and material theme provider

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import { Router, Route, Link, browserHistory } from 'react-router';
 import Home from './component/Home';
 import AddRecipe from './component/AddRecipe';
 import Recipe from './component/Recipe';
 
+injectTapEventPlugin();
+
 ReactDOM.render(
-	<Router history={browserHistory}>
-		<Route path="/" activeClassName="active" component={Home} />
-		<Route path="/create" activeClassName="active" component={AddRecipe} />
-		<Route path="/recipe/:recipeId/:recipeName" activeClassName="active" component={Recipe} />
-	</Router>,
+	<MuiThemeProvider>
+		<Router history={browserHistory}>
+			<Route path="/" activeClassName="active" component={Home} />
+			<Route path="/create" activeClassName="active" component={AddRecipe} />
+			<Route path="/recipe/:recipeId/:recipeName" activeClassName="active" component={Recipe} />
+		</Router>
+	</MuiThemeProvider>,
 	document.getElementById('app')
 );
