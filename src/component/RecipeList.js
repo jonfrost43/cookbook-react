02f674c128be00d357e896b96a3ffffdb556d227
jonfrost43 <jonfrost43@gmail.com>
2016-09-17 16:55:07 +0100

    use Card component for recipes in list

--- src/component/RecipeList.js
+++ src/component/RecipeList.js
@@ -1,15 +1,38 @@
 import React from 'react';
 import { Link } from 'react-router';
+import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
+import { browserHistory } from 'react-router';
 
-export default React.createClass({
+var RecipeListItem = React.createClass({
+	onClickRecipe: function () {
+		var recipe = this.props.recipe;
+		var path = '/recipe/'+recipe.id+'/'+recipe.name.replace(/\s/g, '-').toLowerCase();
+		browserHistory.push(path);
+	},
+	render: function () {
+		var recipe = this.props.recipe;
+		return (
+			<li key={recipe.id} className="cardContainer">
+				<Card containerStyle={{paddingBottom:0}} onClick={this.onClickRecipe}>
+					<CardMedia><img src={recipe.image} /></CardMedia>
+					<CardTitle title={recipe.name} />
+				</Card>
+			</li>
+		);
+	}
+});
+
+var RecipeList = React.createClass({
 	render: function(){
+		var self = this;
 		return (
 			<ul className="recipeList">
 				{this.props.data.map(function(recipe){
-					var path = '/recipe/'+recipe.id+'/'+recipe.name.replace(/\s/g, '-').toLowerCase();
-					return <li key={recipe.id}><Link to={path}>{recipe.name}<img src={recipe.image} /></Link></li>;
+					return <RecipeListItem key={recipe.id} recipe={recipe} />;
 				})}
 			</ul>
 		);
 	}
 });
+
+export default RecipeList;
