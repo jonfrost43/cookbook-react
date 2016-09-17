8cb0d978cf8eb6255df84fb425976d5f7e487a73
jonfrost43 <jonfrost43@gmail.com>
2016-09-17 16:55:39 +0100

    use material-ui form components on create page

--- src/component/Ingredients.js
+++ src/component/Ingredients.js
@@ -1,26 +1,65 @@
 import React from 'react';
+import TextField from 'material-ui/TextField';
+import SelectField from 'material-ui/SelectField';
+import MenuItem from 'material-ui/MenuItem';
+import FlatButton from 'material-ui/FlatButton';
+
+var Ingredient = React.createClass({
+	getInitialState: function(){
+		return {
+			unit: null
+		};
+	},
+
+	onUnitChange: function(evt, index, value){
+		this.setState({
+			unit: value
+		});
+	},
+
+	render: function(){
+		var index = this.props.index;
+		return (
+			<li>
+				<TextField type="number" name={'iQuantity'+index} />
+				<SelectField name={'iUnit'+index} value={this.state.unit} onChange={this.onUnitChange} floatingLabelText="Unit">
+					<MenuItem value={0} primaryText="g" />
+					<MenuItem value={1} primaryText="kg" />
+					<MenuItem value={2} primaryText="oz" />
+					<MenuItem value={3} primaryText="ml" />
+					<MenuItem value={4} primaryText="fl oz" />
+					<MenuItem value={5} primaryText="tbsp" />
+					<MenuItem value={6} primaryText="tsp" />
+				</SelectField>
+				<TextField name={'iText'+index} />
+			</li>
+		);
+	}
+});
 
 var Ingredients = React.createClass({
+	getInitialState: function(){
+		return {
+			ingredients: [{quantity: '', unit: 0, text: ''}]
+		};
+	},
+
+	add: function(){
+		this.setState({
+			ingredients: this.state.ingredients.concat({quantity: '', unit: 0, text: ''})
+		});
+	},
+
 	render: function(){
 		return (
 			<fieldset className="ingredients">
 				<legend>Ingredients</legend>
-				<div>
-					<input type="number" name="iQuantity1" />
-					<select name="iUnit1">
-						<option></option>
-						<option>g</option>
-						<option>kg</option>
-						<option>oz</option>
-						<option>ml</option>
-						<option>fl oz</option>
-						<option>fl oz</option>
-						<option>tbsp</option>
-						<option>tsp</option>
-					</select>
-					<input type="text" name="iText1" />
-				</div>
-				<button type="button">Add ingredient</button>
+				<ol>
+				{this.state.ingredients.map(function(ingredient, index){
+					return <Ingredient key={index} index={index}></Ingredient>;
+				})}
+				</ol>
+				<FlatButton label="Add ingredient" primary={true} onClick={this.add} />
 			</fieldset>
 		);
 	}
--- src/component/RecipeMethod.js
+++ src/component/RecipeMethod.js
@@ -1,15 +1,44 @@
 import React from 'react';
+import TextField from 'material-ui/TextField';
+import FlatButton from 'material-ui/FlatButton';
 
-export default React.createClass({
+var RecipeMethodStep = React.createClass({
+	render: function () {
+		var index = this.props.index;
+		return (
+			<li>
+				<TextField name={'methodStep'+index} multiLine={true} rows={2} />
+			</li>
+		);
+	}
+});
+
+var RecipeMethod = React.createClass({
+	getInitialState: function(){
+		return {
+			steps: [{text: ''}]
+		};
+	},
+
+	add: function(){
+		this.setState({
+			steps: this.state.steps.concat({text: ''})
+		});
+	},
+
 	render: function(){
 		return (
 			<fieldset className="method">
                 <legend>Method</legend>
-                <div>
-                    <textarea name="methodStep1"></textarea>
-                </div>
-                <button type="button">Add step</button>
+				<ol>
+				{this.state.steps.map(function(step, index){
+					return <RecipeMethodStep key={index} index={index}></RecipeMethodStep>;
+				})}
+				</ol>
+				<FlatButton label="Add step" primary={true} onClick={this.add} />
             </fieldset>
 		);
 	}
 });
+
+export default RecipeMethod;
